eb4187131f286e04e7f52353518f54aa58bfbe78
nlukic97 <nlukic97@gmail.com>
2020-06-02 18:26:54 +0200

    first commit

---
+++ src/views/videos.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'Videos',
+  data() {
+    return {
+      videos: {
+        en:[ 
+            {
+              title:'The Man Behind the World’s Ugliest Buildings - Alternatino',
+              id:'uvU5dmu4sl8'
+            },
+            {
+              title:'Shaquille o Neal party at tomorrowland 😂',
+              id:'iT4CEMJNM0c'
+            },
+            {
+              title:'Chappelle\'s Show - The Racial Draft (ft. Bill Burr, RZA, and GZA) - Uncensored',
+              id:'2z3wUD3AZg4'
+            },
+
+          ],
+        rs:[
+              {
+                title:'KAKO GOVORITI SRPSKI JEZIK!!!!!',
+                id:'bAkY3Lm_h0Q'
+              },
+              {
+                title:'24H MENJAM AUTO SA CHODOM ! * udario kontejner *',
+                id:'4i5spo6-ogk'
+              }
+            ]
+      },
+      translations: {
+        mainHeading: {
+          en: 'Welcome to duo-lingual-youtube',
+          sr: 'Dobro dosli na dvojezicni youtube'
+        },
+        listOfVideos: {
+          en: 'List of videos',
+          sr: 'Lista klipova'
+        }
+      }
+    }
+  }
+}
+</script>
